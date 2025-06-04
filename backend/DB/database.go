c02820078a51480568c6613b1fa292da9c84d437
Lazy_dog <2929076584@qq.com>
2025-06-04 14:32:08 +0800

    3.2

--- backend/DB/database.go
+++ backend/DB/database.go
@@ -1,17 +1,29 @@
 package DB
 
 import (
+	"UnoBackend/config"
 	"UnoBackend/internal/model"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "root:qweasd@tcp(119.84.246.217:38158)/table_game?charset=utf8mb4&parseTime=True&loc=Local"
+	dir, _ := os.Getwd()
+	fmt.Println("当前工作目录：", dir)
+	config.LoadConfig("config.yaml") // 从当前目录读取配置
+	dbConfig := config.AppConfig1.Database
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbConfig.User,
+		dbConfig.Password,
+		dbConfig.Host,
+		dbConfig.Port,
+		dbConfig.DBName)
+	//dsn := "root:qweasd@tcp(119.84.246.217:38158)/table_game?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
